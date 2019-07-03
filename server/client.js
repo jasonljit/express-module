99a0c930501cfa975a718ac582b7e3e4e1332e9f
Jason Lin <jasonlin@ljit.io>
2019-07-03 14:29:46 +0800

    add connection logger

--- server/client.js
+++ server/client.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const connectionLogger = require('./middlewares/connection-logger');
 
+app.use(connectionLogger);
+app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', require('./routes/client'));
 
