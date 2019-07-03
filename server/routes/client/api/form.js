22d5147af4db93c0f7c1a719023e9cf241ac96ca
Jason Lin <jasonlin@ljit.io>
2019-07-03 15:09:36 +0800

    fix typo

--- server/routes/client/api/form.js
+++ server/routes/client/api/form.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.post('/', (req, res) => {
-	res.redner('pages/second-page');
+	res.render('pages/second-page');
 });
 
 module.exports = router;
