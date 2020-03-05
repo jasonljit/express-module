f21072603f6117fd57772f247655b5ca489c87f7
jason <jasonlin@ljit.io>
2020-03-05 11:42:15 +0800

    add body parser

--- server/client.js
+++ server/client.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', require('./routes/client'));
+app.use('/api', require('./routes/client-api'));
 
 module.exports = app;
